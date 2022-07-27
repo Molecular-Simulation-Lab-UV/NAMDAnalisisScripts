1be31e4f95b2473fee9be7bce083290ab5458e7d
ThaNikkitz <nico.espinozam@gmail.com>
2022-07-27 12:18:43 -0400

    I think it is working. At least it is clean now.

--- PermeationEvents/permeationEvents.py
+++ PermeationEvents/permeationEvents.py
@@ -7,29 +7,19 @@ going from the inner solvent to the outer solvent throught the pore. To achieve 
 import prody
 import numpy
 import argparse
-import sys
 from datetime import datetime
-
-numpy.set_printoptions(threshold=sys.maxsize)
 
 parser = argparse.ArgumentParser(description = 'Calculate the RMSD Matrix of a trajectory for input in the GROMOS++ clustering program.')
 parser.add_argument('-i', '--in_file', type = str, required = True, help = 'Path, either absolute or relative, to the input file')
-parser.add_argument('-d', '--direction', type = str, required = False, help = 'Count events in the "up" direction (z+), "down" direction (z-), or "both" events. Default = "both"')
 
 arg = parser.parse_args()
 inFile = open(arg.in_file, 'r')
 
-direction = 'both'
-
-if arg.direction == None or arg.direction.lower() == 'both':
-    direction = 'both'
-elif arg.direction.lower() == 'up':
-    direction = 'up'
-elif arg.direction.lower() == 'down':
-    direction = 'down'
-
 dcdName = []
 outName = 'outFile.out'
+
+# Reading inputs from the input parameters file. This is for the analysis calculation,
+# NOT for the simulation itself.
 
 print('\nReading the input and asigning variables')
 
@@ -59,109 +49,68 @@ for line in inFile:
     elif l[0] == 'out':
         outName = l[1]
 
+# Setting up structure and linking the trajectory files to it.
+
 pdb = prody.parsePDB(pdbName)
 traj = prody.Trajectory(dcdName[0])
-
 if len(traj) > 1:
     for d in dcdName[1:]:
         traj.addFile(d)
 
-if direction == 'both':
-    NN = 3
-elif direction == 'up' or direction == 'down':
-    NN = 1
-
 traj.link(pdb)
 traj.setCoords(pdb)
-traj.setAtoms(pdb.select(refName))
+traj.setAtoms(pdb.select(refName)) # refName = Selection used when aligning frames (frame.superpose())
 atomsSelection = pdb.select(selName)
 
-# Initialise the dictionary with zeros. Values will change when permeation events are detected, and all three are reset to 0.
-# 4 is for extracellular, 2 for inside the cylinder, 1 for intracellular.
-dataDict = {}
-atomIDs = atomsSelection.getIndices()
-for ind in atomIDs:
-    dataDict[ind] = 0
+NN = 3
 
+atomIDs = atomsSelection.getIndices() # "sel" selection. Usually would be "name OH2".
+
+"""
+flags and flagsOld hold the indicators for signaling in which region the atom is.
+1: z < 0 and outside the cylinder.
+3: atom inside the cylinder.
+6: z > 0 and outside the cylinder.
+flags holds the indicator (flag) for each atom at the current frame/timestep.
+flagsOld holds the previous 2 indicators - shape (len(traj), 2). Only changes if its [i-th, -1] value differs from the [i-th] value from the 'flags' array.
+The corresponding element [frameNumber, -1] gets updated when it differs from the respective one from 'flags'.
+permArray simply holds the frame and the counts in each direction, as well as the total.
+diffArray holds the 'flags' ARRAY's index (not the atom's index!) of the differing flags.
+"""
 flagsOld = numpy.zeros((len(atomsSelection), 2))
 permArray = numpy.zeros((len(traj), NN)) # Array containing the counters for permeation in each direction, according to NN (line 67 of the script)
 permArray = numpy.hstack((numpy.linspace(1, len(traj), len(traj))[:,numpy.newaxis], permArray)).astype('int')
 
-
-# def getOldPos():
-#     atomIDs = pdb.select(selName).getIndices()
-#     zetas = pdb[atomIDs].getCoords()[:,2] # Get z coordinate for all elements in the selection.
-#     Arr = numpy.vstack((atomIDs, zetas))
-#     return Arr
-
 t1 = datetime.now()
 
-if direction == 'up':
-    counter = 0
-    for i, frame in enumerate(traj):
-        frame.superpose()
-        posArray = atomsSelection.getCoords()
-        flags = numpy.where(posArray[:,2] < 0, numpy.ones(len(posArray)), 6*numpy.ones(len(posArray)))
-        flags = numpy.where((numpy.sqrt(posArray[:,0]**2 + posArray[:,1]**2) < rad) & (posArray[:,2] < upZ) & (posArray[:,2] > loZ), 3*numpy.ones(len(posArray)), flags)
-        flags = flags.T
+counter = [0, 0]
+for i, frame in enumerate(traj):
+    frame.superpose()
+    posArray = atomsSelection.getCoords()
+    # Flag atoms with postion z > 0 with 6, and z < 0 with 1
+    flags = numpy.where(posArray[:,2] < 0, numpy.ones(len(posArray)), 6*numpy.ones(len(posArray)))
+    # Include the flags for the atoms inside the cylinder
+    flags = numpy.where((numpy.sqrt(posArray[:,0]**2 + posArray[:,1]**2) < rad) & (posArray[:,2] < upZ) & (posArray[:,2] > loZ), 3*numpy.ones(len(posArray)), flags)
+    flags = flags.T # Transpose the flags array to get it from shape (, len(posArray)) to (len(posArray),).
+    
+    diffArray = numpy.concatenate(numpy.argwhere(flags - flagsOld[:,-1] != 0)) # Explained a while back.
+    for value in diffArray:
+        if 2 - flagsOld[value, -2] + flags[value] == 7:
+            counter[0] += 1
+        elif 2 - flagsOld[value, -2] + flags[value] == -3:
+            counter[1] += 1
+            
+            # Update the counters and flagsOld's flags.
 
-        diffArray = numpy.concatenate(numpy.argwhere(flags - flagsOld[:,-1] != 0))
-
-        for value in diffArray:
-            if 1 + numpy.sum((flagsOld[value], flags[value])) == 7:
-                counter += 1
-                permArray[i,-1] = counter
-            numpy.roll(flagsOld[value],-1)
-            flagsOld[value,-1] = flags[value]
-
-        
-elif direction == 'down':
-    counter = 0
-    for i, frame in enumerate(traj):
-        frame.superpose()
-        posArray = atomsSelection.getCoords()
-        flags = numpy.where(posArray[:,2] < 0, numpy.ones(len(posArray)), 6*numpy.ones(len(posArray)))
-        flags = numpy.where((numpy.sqrt(posArray[:,0]**2 + posArray[:,1]**2) < rad) & (posArray[:,2] < upZ) & (posArray[:,2] > loZ), 3*numpy.ones(len(posArray)), flags)
-        flags = flags.T
-
-        diffArray = numpy.concatenate(numpy.argwhere(flags - flagsOld[:,-1] != 0))
-
-        for value in diffArray:
-            if 1 + numpy.sum((flagsOld[value], flags[value])) == -3:
-                counter += 1
-                permArray[i, -1] = counter
-            numpy.roll(flagsOld[value],-1)
-            flagsOld[value,-1] = flags[value]
-
-elif direction == 'both':
-    counter = [0, 0]
-    for i, frame in enumerate(traj):
-        frame.superpose()
-        posArray = atomsSelection.getCoords()
-        flags = numpy.where(posArray[:,2] < 0, numpy.ones(len(posArray)), 6*numpy.ones(len(posArray)))
-        flags = numpy.where((numpy.sqrt(posArray[:,0]**2 + posArray[:,1]**2) < rad) & (posArray[:,2] < upZ) & (posArray[:,2] > loZ), 3*numpy.ones(len(posArray)), flags)
-        flags = flags.T
-
-        diffArray = numpy.concatenate(numpy.argwhere(flags - flagsOld[:,-1] != 0))
-
-        for value in diffArray:
-            if 2 - flagsOld[value, -2] + flags[value] == 7:
-                counter[0] += 1
-            elif 2 - flagsOld[value, -2] + flags[value] == -3:
-                counter[1] += 1
-
-            permArray[i,-3] = counter[1] # Direction -z
-            permArray[i,-2] = counter[0] # Direction +z
-            permArray[i,-1] = counter[0] + counter[1] # Total count
-            flagsOld[value] = numpy.roll(flagsOld[value],-1)
-            flagsOld[value,-1] = flags[value]
-
-else:
-    print('PROBLEM: Direction incorrect!')
-    exit()
+        permArray[i,-3] = counter[1] # Direction -z
+        permArray[i,-2] = counter[0] # Direction +z
+        permArray[i,-1] = counter[0] + counter[1] # Total count
+        # Push flagsOld flags (for the specific atomIndex) to make place for the updated one
+        flagsOld[value] = numpy.roll(flagsOld[value],-1)
+        flagsOld[value,-1] = flags[value] # Update flags from flagsOld
 
 
-permArray = permArray.astype('str')
+permArray = permArray.astype('str') # Handy for writing to a file
 
 outFile = open(outName, 'w+')
 if direction == 'up':
@@ -178,17 +127,5 @@ outFile.close()
 
 t2 = datetime.now()
 
+print('\nFIN')
 print('Time to completion was {0}'.format((t2.replace(microsecond=0) - t1.replace(microsecond=0))))
-
-    # Ahora tengo que cambiar las flags de los índices que están dentro del cilindro SI ES QUE CORRESPONDE.
-
-    # flags = numpy.where(z < loZ, numpy.ones(len(z)), numpy.zeros(len(z)))
-    # flags = numpy.where(z > upZ, 4*numpy.ones(len(z)), flags)
-    # flags = numpy.where(flags == 0, 2*numpy.ones(len(z)), flags)
-    # for j in diffArray:
-    #     for k in range(3):
-    #         if dataDict[atomIDs[j]][k] != 0 and dataDict[atomIDs[j]][k] != flags[j]:
-    #             dataDict[atomIDs[j]] = flags[j]
-
-
-
