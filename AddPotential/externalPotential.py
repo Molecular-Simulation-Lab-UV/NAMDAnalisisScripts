69bbc0950e0b48a69184c40f8576b5cc25428052
Nicolas Espinoza <nico.espinozam@gmail.com>
2023-03-01 10:53:49 -0300

    Polished the code a little. No rigorous tests have been run

--- AddPotential/externalPotential.py
+++ AddPotential/externalPotential.py
@@ -6,15 +6,16 @@ TODO: add the axis option
 import gridData, argparse, numpy
 
 parser = argparse.ArgumentParser(description = "Add the effect of an external potential to the .dx file")
-parser.add_argument('-i', '--in_file', type = str, required = True, help = 'Path, either absolute or relative, to the dx file')
-parser.add_argument('-f', '--field', type = float, required = True, help = 'Value, in mV, of the external potential. E.g: -100')
-parser.add_argument('-o', 'out', type = str, required = False, help = 'Output path of the resulting modified DX file. ./outputDX.dx by default')
+parser.add_argument('-i', '--in_file', type = str, required = True, help = '[String] Path, either absolute or relative, to the dx file')
+parser.add_argument('-f', '--field', type = float, required = True, help = '[Float] Value, in mV, of the external potential. E.g: -100')
+parser.add_argument('-t', '--temp', type = float, required = False, help = '[Float] Temperature, in Kelvin, at which the simulation is run. Used to calculate potential in k_BT/e. Default = 300')
+parser.add_argument('-o', '--out', type = str, required = False, help = '[String] Output path of the resulting modified DX file. ./outputDX.dx by default')
 
 arg = parser.parse_args()
 dx = gridData.Grid(arg.in_file)
 finalDX = dx.grid.copy()
 
-cF = 1/25.855 # Conversion factor @ 300 Kelvin
+cF = 1/(0.0862*arg.temp) # Conversion factor @ 300 Kelvin
 
 if arg.field > 0:
     # FALTA TRANSFORMARLO A UNIDADES NAMD
