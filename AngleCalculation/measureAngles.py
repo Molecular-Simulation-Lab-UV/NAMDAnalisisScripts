b6bc769ea04c81598615e8f9f424058e41d4717c
Nicolas Espinoza <nico.espinozam@gmail.com>
2024-07-07 22:30:42 -0400

    Avoiding chain/segment problems The script is now a little faster, at least with the tested systems, and it bypasses problems with pdbs not containing either chain or segment info. It achieves this by simply filling this information with a PRO+chainID, or the last char of the PROX. It might not work in ALL cases though.

--- AngleCalculation/measureAngles.py
+++ AngleCalculation/measureAngles.py
@@ -1,8 +1,7 @@
 import prody
-import socket
 from datetime import datetime
 import argparse
-import numpy
+import numpy as np
 
 angle = 'both'
 
@@ -32,12 +31,7 @@ for line in inFile:
         elif 'sel' in l[0]:
             selName = ' '.join(l[1:])
         elif 'ref' in l[0]:
-            if len(l[1:]) > 1:
-                refSel = ' '.join(l[1:])
-            else:
-                refSel = l[1]
-        elif 'chain' in l[0]:
-            chainID = l[1]
+            refSel = ' '.join(l[1:])
         elif 'pdb' in l[0]:
             pdbName = l[1]
         elif 'out' in l[0]:
@@ -49,54 +43,56 @@ inFile.close()
 
 pdb = prody.parsePDB(pdbName)
 traj = prody.Trajectory(dcdName[0])
-
-if 'resid ' not in selName:
-    selName = 'protein and chain ' + chainID + ' and resid ' + selName
-elif 'resid' in selName:
-    selName = 'protein and chain ' + chainID + ' and ' + selName
-
-print('Starting calculations and setting up stuff. You\'re free to go make some coffee or something')
-t1 = datetime.now()
-
-# Check for unique residues and split the measurement
-selUniverse = pdb.getHierView()
-unis = numpy.unique(pdb.select(selName).getResnums())
-
-# If there are more dcd files, add them to the trajectory
 if len(dcdName) > 1:
     for dcd in dcdName[1:]:
         traj.addFile(dcd)
-
 traj.link(pdb)
 traj.setCoords(pdb)
 traj.setAtoms(pdb.select(refSel))
 
-#Arrays for storing the calculated values
+print('Starting calculations and setting stuff up. You\'re free to go make some coffee or something')
 
-if len(traj) > 1e4 and len(unis) > 3 and angle == 'both' and socket.gethostname() != 'don-elias':
-    print('Better run this on the server. Otherwise memory will probably go (╯°□°)╯︵ ┻━┻ \n')
-    print('Recommendation is to Ctrl + C this process and run it on don-elias.')
+# Check for unique residues and split the measurement
+sel = pdb.select(selName)
+view = sel.getHierView()
+unis = np.unique(sel.getResnums())
+if (np.unique(sel.getSegnames()) == '').all():
+    for chain in np.unique(sel.getChids()):
+        pdb.select('chain ' + chain).setSegnames('PRO' + chain)
+elif (np.unique(sel.getChids()) == '').all():
+    for seg in np.unique(sel.getSegnames()):
+        pdb.select('segment ' + seg).setChids(seg[-1])
 
-dih = numpy.zeros((len(traj), len(unis))) 
+segment = np.unique(sel.getSegnames())[0]
+chain = np.unique(sel.getChids())[0]
+
+if view.numSegments() > 1 or view.numChains() > 1:
+    print('Please run a single job per segment/chain. Modify your selection and try again')
+    exit()
+
+t1 = datetime.now()
+sel2 = pdb[segment, chain]
+# Arrays for storing the calculated values
+dih = np.zeros((len(traj), len(unis))) 
 if angle == 'both':
-    dih2 = numpy.zeros((len(traj), len(unis)))
+    dih2 = np.zeros((len(traj), len(unis)))
 
 if angle == 'phi':
     for i, frame in enumerate(traj):
         frame.superpose()
         for j, res in enumerate(unis):
-            dih[i, j] = prody.calcPhi(selUniverse[chainID,chainID,res],dist=None)
+            dih[i, j] = prody.calcPhi(sel2[res,], dist=None)
 elif angle == 'psi':
     for i, frame in enumerate(traj):
         frame.superpose()
         for j, res in enumerate(unis):
-            dih[i, j] = prody.calcPsi(selUniverse[chainID,chainID,res],dist=None)
+            dih[i, j] = prody.calcPsi(sel2[res,], dist=None)
 else:
     for i, frame in enumerate(traj):
         frame.superpose()
         for j, res in enumerate(unis):
-            dih[i, j] = prody.calcPhi(selUniverse[chainID,chainID,res],dist=None)
-            dih2[i, j] = prody.calcPsi(selUniverse[chainID,chainID,res],dist=None)
+            dih[i, j] = prody.calcPhi(sel2[res,], dist=None)
+            dih2[i, j] = prody.calcPsi(sel2[res,], dist=None)
 
 # Cast as string to be able to write to file easier
 dih = dih.astype('str')
@@ -113,7 +109,9 @@ else:
 	ending = '.' + outName.split('.')[-1]
 
 outName = outName.split(ending)[0]
+
 if angle != 'both':
+    print('\nWriting to output file:\t' + outName + '.' + angle + ending + '\n')
     outFile = open(outName + '.' + angle + ending, 'w+')
     outFile.write('#Frame \t resids {0} \n'.format('\t '.join(unis)))
     for k, vals in enumerate(dih):
@@ -121,12 +119,14 @@ if angle != 'both':
     outFile.close()
 else:
     # Write phi bond angles
+    print('\nWriting to output file:\t' + outName + '.' + 'phi' + ending)
     outFile1 = open(outName + '.phi' + ending, 'w+')
     outFile1.write('#Frame \t resids {0} \n'.format('\t '.join(unis)))
     for k, vals in enumerate(dih):
         outFile1.write('{0:7} \t {1} \n'.format(k, '\t '.join(vals)))
     outFile1.close()
     # Write psi bond angles
+    print('Writing to output file:\t' + outName + '.' + 'psi' + ending + '\n')
     outFile2 = open(outName + '.psi' + ending, 'w+')
     outFile2.write('#Frame \t resids {0} \n'.format('\t '.join(unis)))
     for k, vals in enumerate(dih2):
